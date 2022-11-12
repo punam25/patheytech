ee567ba4d1e874276a6b9ca10d258c39c632e2ce
punam <punam.patheytech@outlook.com>
2022-11-12 16:22:55 +0530

    javascript assignment

---
+++ pathey-assignment/index.js
@@ -0,0 +1,31 @@
+
+    // console.log("welcome home");/
+    // there are 3 variable declaration var let const
+    var x=10;
+    var y=20;
+    var result= x+y;
+    console.log('result=>',result);
+    // use var the declartion and value allow to repeat
+
+    let a=3;
+    let b=5;
+    let z=3+6;
+    console.log(z);
+    // Variables defined with let cannot be redeclared.
+
+    const d=10;
+    //const is not declaring there declartion and value
+
+    //function declaration
+     function firstprogram(){
+        console.log('hello world');
+     }
+     firstprogram();
+
+     // Function invocation from Button/Link
+     function displaymessage(){
+         document.getElementById('msg').innerHTML= "the button has been clicked";
+        alert('button clicked');
+     }
+
+    
